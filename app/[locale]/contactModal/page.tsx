f2b9230bcedad8e05049333b77b37b5af34f7a19
Aghiles-Belkalem <belkalemaghiles@gmail.com>
2025-08-20 22:24:43 +0100

    MVP completed first vresion of the project fixing issues on modal due to build 2

---
+++ app/[locale]/contactModal/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import ContactForm from "@/components/ui/contact/contactForm";
+
+export default function ContactModalPage() {
+  return (
+    <main style={{ padding: "2rem" }}>
+      <h1>Contact</h1>
+      <ContactForm />
+    </main>
+  );
+}
--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -6,42 +6,15 @@ import { useRouter, usePathname } from 'next/navigation';
 import ProjectsSection from '@/components/ui/projet/projets';
 
 export default function HomePage() {
-  const { t, locale } = useTranslations();
+  useTranslations();
   const router = useRouter();
   const pathname = usePathname();
 
-  const handleLocaleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    const newLocale = e.target.value;
-    // Remplace le segment de langue au début de l’URL
-    const newPath = pathname.replace(/^\/[a-z-]+/, `/${newLocale}`);
-    router.push(newPath);
-  };
 
   return (
     <>
-      {/* Hero Section */}
-      <HeroLanding />
-
-      {/* Sélecteur de langue */}
-      <div style={{ textAlign: "right", margin: "1rem" }}>
-        <label htmlFor="locale" style={{ marginRight: "0.5rem" }}>
-          🌍 {t.header?.language || "Langue"} :
-        </label>
-        <select
-          id="locale"
-          value={locale}
-          onChange={handleLocaleChange}
-          style={{ padding: "0.3rem", borderRadius: "6px" }}
-        >
-          <option value="fr">🇫🇷 Français</option>
-          <option value="en">🇬🇧 English</option>
-          <option value="pt">🇵🇹 Português</option>
-        </select>
-      </div>
-
-      {/* Section Projets */}
-      <ProjectsSection />
+      <HeroLanding/>
+      <ProjectsSection/>
     </>
   );
 }
-
